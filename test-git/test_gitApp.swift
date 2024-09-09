b98f020101fd885fe9a9e643fcb6268ff5487624
Matthew Dong <mattdong1231@gmail.com>
2024-09-09 15:54:25 -0400

    Initial Commit

---
+++ test-git/test_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_gitApp.swift
+//  test-git
+//
+//  Created by Matthew Dong on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct test_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
